e793bc16daca3a765b7407344fac53fb82f2c506
mirco.cennamo <mirco.cennamo@avanade.com>
2024-02-29 15:24:23 +0100

    foreign server

--- senderRestTemplateWithServiceDiscovery/src/main/java/com/avanade/SenderRestTemplateWithServiceDiscoveryApplication.java
+++ senderRestTemplateWithServiceDiscovery/src/main/java/com/avanade/SenderRestTemplateWithServiceDiscoveryApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SenderRestTemplateApplication {
+public class SenderRestTemplateWithServiceDiscoveryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SenderRestTemplateApplication.class, args);
+        SpringApplication.run(SenderRestTemplateWithServiceDiscoveryApplication.class, args);
     }
 }
