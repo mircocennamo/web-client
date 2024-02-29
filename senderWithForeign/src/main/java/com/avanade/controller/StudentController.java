6228910ab342a25c091bbe10df03aa46700063ae
mirco.cennamo <mirco.cennamo@avanade.com>
2024-02-29 15:07:39 +0100

    foreign server

--- senderWithForeign/src/main/java/com/avanade/controller/StudentController.java
+++ senderWithForeign/src/main/java/com/avanade/controller/StudentController.java
@@ -1,28 +1,30 @@
 package com.avanade.controller;
 
+
 import com.avanade.model.Student;
 import com.avanade.service.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/student")
-public class ReceiverController {
+@RequestMapping("/students")
+public class StudentController {
 
-    Logger logger = LoggerFactory.getLogger(ReceiverController.class);
+    Logger logger = LoggerFactory.getLogger(StudentController.class);
     
-   @Autowired
     private StudentService studentService;
     
-   @GetMapping("/{id}")
-    public Student getStudentId(@PathVariable Long id) {
-
-       logger.debug("called getStudentId with id {} " , id);
-       return studentService.getStudent(id);
+    StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    
+    @GetMapping("/{id}")
+    public Student getStudentId(@PathVariable int id) {
+        logger.debug("SenderController called getStudentId with id {} " , id);
+        return studentService.getStudentId(id);
     }
 }
---
+++ senderWithForeign/src/main/java/com/avanade/model/Student.java
@@ -0,0 +1,29 @@
+package com.avanade.model;
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Student {
+
+
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String passportNumber;
+
+    public Student(String name, String passportNumber) {
+        this.name = name;
+        this.passportNumber = passportNumber;
+    }
+}
